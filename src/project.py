6821126a36e02c4b7f66158484326ab4044ce074
MeerKateAnimation <kat@blueeyedzoo.com>
2023-11-15 14:58:06 -0600

    create project.py and write pseudocode

---
+++ src/project.py
@@ -0,0 +1,21 @@
+import maya as cmds
+
+
+#read selected meshes and create some if none are selected
+#make a pop-up window with options to customize rig
+#calculate curve points and joints amount
+#create curve
+#skin curve to joints
+#create animation controls
+#constrain joints to controls
+#calculate distance between chain links
+#constrain chains to curve
+#make a control to limit each chain's distance from each other link
+#write docstrings? 
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
